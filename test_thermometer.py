86e755e2196fabb5edbb8b401285c46c244543e7
Michael Guterl <michael@diminishing.org>
2013-09-24 08:56:22 -0400

    Convert tests to use nose

--- test_thermometer.py
+++ test_thermometer.py
@@ -1,5 +1,3 @@
-import unittest
-
 from sensor import Sensor
 from temperature import Temperature
 from connection import Connection
@@ -29,33 +27,29 @@ class FakeSerialPort:
   def __reset__(self):
     self.next_response = FakeSerialPort.NO_RESPONSE
 
-class ThermometerTest(unittest.TestCase):
+def test_temperature_equality():
+  t1 = Temperature(70.0)
+  t2 = Temperature(70.0)
+  assert t1 == t2
 
-  def test_temperature_equality(self):
-    t1 = Temperature(70.0)
-    t2 = Temperature(70.0)
-    self.assertEqual(t1, t2)
+def test_temperature_conversion_to_celcius():
+  temperature = Temperature(68.0)
+  celcius = 20.0
+  assert celcius == temperature.celcius
 
-  def test_temperature_conversion_to_celcius(self):
-    temperature = Temperature(68.0)
-    celcius = 20.0
-    self.assertEqual(celcius, temperature.celcius)
+def test_temperature_conversion_from_string():
+  temperature = Temperature("68.0")
+  assert 68.0 == temperature.farenheit
 
-  def test_temperature_conversion_from_string(self):
-    temperature = Temperature("68.0")
-    self.assertEqual(68.0, temperature.farenheit)
+def test_current_temperature():
+  connection = FakeConnection({ "temperature:current": "72.0" })
+  sensor = Sensor(connection)
+  temperature = sensor.current_temperature()
+  assert Temperature(72.0) == temperature
 
-  def test_current_temperature(self):
-    connection = FakeConnection({ "temperature:current": "72.0" })
-    sensor = Sensor(connection)
-    temperature = sensor.current_temperature()
-    self.assertEqual(Temperature(72.0), temperature)
+def test_connection():
+  fake_serial_port = FakeSerialPort()
+  connection = Connection(fake_serial_port)
+  response = connection.send("temperature:current")
+  assert "72.0" == response
 
-  def test_connection(self):
-    fake_serial_port = FakeSerialPort()
-    connection = Connection(fake_serial_port)
-    response = connection.send("temperature:current")
-    self.assertEqual("72.0", response)
-
-if __name__ == '__main__':
-  unittest.main()
