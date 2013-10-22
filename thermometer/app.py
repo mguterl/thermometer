7e1257f1efa7ccee53d7bc97822cdd9e8bee1d7a
Michael Guterl <michael@diminishing.org>
2013-10-21 23:33:54 -0400

    Only assign fields in the App constructor

--- thermometer/app.py
+++ thermometer/app.py
@@ -1,10 +1,17 @@
 import thermometer
 
 class App:
-  def __init__(self):
-    self.db = thermometer.Database('thermometer.db')
-    self.store = thermometer.Store(self.db)
-    self.sensor = thermometer.sensor('/dev/ttyACM0')
+  @classmethod
+  def default(cls):
+    database = thermometer.Database('thermometer.db')
+    store = thermometer.Store(database)
+    sensor = thermometer.sensor('/dev/ttyACM0')
+
+    return cls(store, sensor)
+
+  def __init__(self, store, sensor):
+    self.store  = store
+    self.sensor = sensor
 
   def record(self):
     current_temperature = self.current_temperature()
