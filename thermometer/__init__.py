bc3e5113d31e34a72cf8d80a1fb61329210579bb
Michael Guterl <michael@diminishing.org>
2013-10-21 22:26:47 -0400

    Move Connection to separate file

--- thermometer/__init__.py
+++ thermometer/__init__.py
@@ -2,6 +2,7 @@
 
 import serial
 from database import Database
+from connection import Connection
 
 def sensor(serial_port):
   serial_port = serial.Serial(serial_port, 9600)
@@ -9,19 +10,6 @@ def sensor(serial_port):
   connection = Connection(serial_port)
   return Sensor(connection)
 
-class Connection:
-  def __init__(self, serial_port):
-    self.serial_port = serial_port
-  def send(self, command):
-    self.write(command)
-    return self.read()
-  def write(self, command):
-    self.serial_port.write("{command}\n".format(command=command))
-  def read(self):
-    output = self.serial_port.readline()
-    return self.parse(output)
-  def parse(self, value):
-    return value.rstrip().split(':')[-1]
 
 class Sensor:
   def __init__(self, connection):
