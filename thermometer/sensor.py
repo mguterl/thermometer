fa1d1efa933b8b0a88ba231e8e0d5d2f61b6ee91
Michael Guterl <michael@diminishing.org>
2013-11-14 21:25:48 -0500

    Add default clock to constructor

--- thermometer/sensor.py
+++ thermometer/sensor.py
@@ -1,7 +1,8 @@
 from thermometer import Temperature
+from datetime import datetime
 
 class Sensor:
-  def __init__(self, connection, clock):
+  def __init__(self, connection, clock = datetime):
     self.connection = connection
     self.clock = clock
 
